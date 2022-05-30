e96de2ef2880dce65a63df82ab3f16c98d7ee8d6
kotvkaske <kamenev-vova@mail.ru>
2022-05-30 15:28:25 +0700

    added unet+segnet

---
+++ detection_models.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+from torch import nn
+from imutils.video import FPS
+from torchvision import models
+import torch
+
+class ModelDetection:
+    def __init__(self, image_size: tuple, model_path='model_path', model_input_size=300):
+        self.model_path = model_path
+        self.model = cv2.dnn.readNetFromCaffe(f'{self.model_path}/architecture.txt',
+                                              f'{self.model_path}/weights.caffemodel')
+        self.model.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        self.model.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        self.model_input_size = model_input_size
+        self.image_size = image_size
+
+    def face_detection(self, image,display_confidence = True):
+        blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), crop=False)
+        w = self.image_size[0]
+        h = self.image_size[1]
+        self.model.setInput(blob)
+        detections = self.model.forward()
+        for i in range(0, detections.shape[2]):
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            confidence = detections[0, 0, i, 2]
+            my_box = box.copy()
+            if confidence > 0.7:
+                my_box = image[startY:endY, startX:endX, :]
+                try:
+                    my_box = cv2.resize(my_box, dsize=(160, 195), interpolation=cv2.INTER_CUBIC)
+                    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+                    if display_confidence:
+                        cv2.putText(image, str(confidence), (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
+                except:
+                    continue
+        return image, my_box
+
+
---
+++ segmentation_models.py
@@ -0,0 +1,354 @@
+import os
+import torch
+import torch.nn.functional as F
+import cv2
+from torch import nn
+from imutils.video import FPS
+from torchvision import models
+import numpy as np
+DEVICE=torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+class SegNet(nn.Module):
+    """Архитектура SegNet для бинарной сегментации (0 - фон, 1 - человек)"""
+
+    def __init__(self):
+        super().__init__()
+        self.encoder_conv_00 = nn.Sequential(*[
+            nn.Conv2d(in_channels=3,
+                      out_channels=64,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(64)
+        ])
+        self.encoder_conv_01 = nn.Sequential(*[
+            nn.Conv2d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(64)
+        ])
+        self.encoder_conv_10 = nn.Sequential(*[
+            nn.Conv2d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(128)
+        ])
+        self.encoder_conv_11 = nn.Sequential(*[
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(128)
+        ])
+        self.encoder_conv_20 = nn.Sequential(*[
+            nn.Conv2d(in_channels=128,
+                      out_channels=256,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.encoder_conv_21 = nn.Sequential(*[
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.encoder_conv_22 = nn.Sequential(*[
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.encoder_conv_30 = nn.Sequential(*[
+            nn.Conv2d(in_channels=256,
+                      out_channels=512,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(512)
+        ])
+        self.encoder_conv_31 = nn.Sequential(*[
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(512)
+        ])
+        self.encoder_conv_32 = nn.Sequential(*[
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=3,
+                      padding=1),
+            nn.BatchNorm2d(512)
+        ])
+
+        #### DECODER
+        self.decoder_convtr_32 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=512,
+                               out_channels=512,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(512)
+        ])
+        self.decoder_convtr_31 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=512,
+                               out_channels=512,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(512)
+        ])
+        self.decoder_convtr_30 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=512,
+                               out_channels=256,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.decoder_convtr_22 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=256,
+                               out_channels=256,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.decoder_convtr_21 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=256,
+                               out_channels=256,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(256)
+        ])
+        self.decoder_convtr_20 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=256,
+                               out_channels=128,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(128)
+        ])
+        self.decoder_convtr_11 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=128,
+                               out_channels=128,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(128)
+        ])
+        self.decoder_convtr_10 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=128,
+                               out_channels=64,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(64)
+        ])
+        self.decoder_convtr_01 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=64,
+                               out_channels=64,
+                               kernel_size=3,
+                               padding=1),
+            nn.BatchNorm2d(64)
+        ])
+        self.decoder_convtr_00 = nn.Sequential(*[
+            nn.ConvTranspose2d(in_channels=64,
+                               out_channels=1,
+                               kernel_size=3,
+                               padding=1)
+        ])
+
+    def forward(self, x):
+        # encoder
+        dim_0 = x.size()
+        x_00 = F.relu(self.encoder_conv_00(x))
+        x_01 = F.relu(self.encoder_conv_01(x_00))
+        x_0, indices_0 = F.max_pool2d(x_01, kernel_size=2, stride=2, return_indices=True)
+
+        # Encoder Stage - 2
+        dim_1 = x_0.size()
+        x_10 = F.relu(self.encoder_conv_10(x_0))
+        x_11 = F.relu(self.encoder_conv_11(x_10))
+        x_1, indices_1 = F.max_pool2d(x_11, kernel_size=2, stride=2, return_indices=True)
+
+        # Encoder Stage - 3
+        dim_2 = x_1.size()
+        x_20 = F.relu(self.encoder_conv_20(x_1))
+        x_21 = F.relu(self.encoder_conv_21(x_20))
+        x_22 = F.relu(self.encoder_conv_22(x_21))
+        x_2, indices_2 = F.max_pool2d(x_22, kernel_size=2, stride=2, return_indices=True)
+
+        # Encoder Stage - 4
+        dim_3 = x_2.size()
+        x_30 = F.relu(self.encoder_conv_30(x_2))
+        x_31 = F.relu(self.encoder_conv_31(x_30))
+        x_32 = F.relu(self.encoder_conv_32(x_31))
+        x_3, indices_3 = F.max_pool2d(x_32, kernel_size=2, stride=2, return_indices=True)
+
+        # Decoder Stage - 4
+        x_3d = F.max_unpool2d(x_3, indices_3, kernel_size=2, stride=2, output_size=dim_3)
+        x_32d = F.relu(self.decoder_convtr_32(x_3d))
+        x_31d = F.relu(self.decoder_convtr_31(x_32d))
+        x_30d = F.relu(self.decoder_convtr_30(x_31d))
+        dim_3d = x_30d.size()
+
+        # Decoder Stage - 3
+        x_2d = F.max_unpool2d(x_30d, indices_2, kernel_size=2, stride=2, output_size=dim_2)
+        x_22d = F.relu(self.decoder_convtr_22(x_2d))
+        x_21d = F.relu(self.decoder_convtr_21(x_22d))
+        x_20d = F.relu(self.decoder_convtr_20(x_21d))
+        dim_2d = x_20d.size()
+
+        # Decoder Stage - 2
+        x_1d = F.max_unpool2d(x_20d, indices_1, kernel_size=2, stride=2, output_size=dim_1)
+        x_11d = F.relu(self.decoder_convtr_11(x_1d))
+        x_10d = F.relu(self.decoder_convtr_10(x_11d))
+        dim_1d = x_10d.size()
+
+        # Decoder Stage - 1
+        x_0d = F.max_unpool2d(x_10d, indices_0, kernel_size=2, stride=2, output_size=dim_0)
+        x_01d = F.relu(self.decoder_convtr_01(x_0d))
+        x_00d = self.decoder_convtr_00(x_01d)
+
+        return x_00d
+
+    def foreground_extraction(self, picture):
+        w = picture.shape[1]
+        h = picture.shape[0]
+        pic = picture
+
+        pic = cv2.resize(pic, dsize=(320, 240), interpolation=cv2.INTER_CUBIC).transpose(2, 0, 1)
+        pic = torch.tensor(pic) / 255
+        result = self.forward(pic.to(DEVICE).unsqueeze(dim=0)).squeeze(dim=0).detach().cpu()
+        result = 255 * (result > 0.5).squeeze(dim=0).numpy().astype(np.uint8)
+        pic = np.array(255 * pic).astype(np.uint8).transpose(1, 2, 0)
+        pic[result == 0] = 0
+        pic = cv2.resize(pic, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+        return pic
+
+
+class convblock(nn.Module):
+    def __init__(self, in_channels, mid_channels, out_channels, kernel=3, padding=0, stride=1):
+        super(convblock, self).__init__()
+        self.first_conv = nn.Sequential(
+            *[nn.Conv2d(in_channels, mid_channels, kernel_size=kernel, padding=padding, stride=stride),
+              nn.BatchNorm2d(mid_channels),
+              nn.BatchNorm2d(mid_channels),
+              nn.ReLU(),
+              nn.Conv2d(mid_channels, out_channels, kernel_size=kernel),
+              nn.BatchNorm2d(out_channels),
+              nn.ReLU()])
+
+    def forward(self, x):
+        x = self.first_conv(x)
+        return x
+
+
+def copy_crop_concat(x1, x2):  ##
+    diffY = x2.size()[2] - x1.size()[2]
+    diffX = x2.size()[3] - x1.size()[3]
+    x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                    diffY // 2, diffY - diffY // 2])
+
+    x = torch.cat([x2, x1], dim=1)
+    return x
+
+
+class UNET_custom(nn.Module):
+    def __init__(self):
+        super(UNET_custom, self).__init__()
+        self.first_block = convblock(3, 64, 64, padding=2)
+        self.first_max_pool = nn.MaxPool2d(2, ceil_mode=True)
+
+        self.second_block = convblock(64, 128, 128, padding=2)
+        self.second_block_pool = nn.MaxPool2d(2, ceil_mode=True)
+
+        self.third_block = convblock(128, 256, 256, padding=2)
+        self.third_block_pool = nn.MaxPool2d(2, ceil_mode=True)
+
+        self.forth_block = convblock(256, 512, 512, padding=2)
+        #        self.forth_block_pool = nn.MaxPool2d(2, ceil_mode=True)
+
+        self.forth_deconvblock = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.forth_convblock = convblock(512, 256, 256, padding=2)
+
+        self.third_deconvblock = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.third_convblock = convblock(256, 128, 128, padding=2)
+
+        self.second_deconvblock = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.second_convblock = convblock(128, 64, 64, padding=2)
+
+        self.outp_layer = nn.Conv2d(64, 1, kernel_size=1)
+
+    def forward(self, x):
+        x = self.first_block(x)
+        x1 = x.clone()
+        x = self.first_max_pool(x)
+        #        print(x.shape, 'спуск 1')
+        x = self.second_block(x)
+        x2 = x.clone()
+        x = self.second_block_pool(x)
+        #        print(x.shape, 'спуск 2')
+        x = self.third_block(x)
+        x3 = x.clone()
+        x = self.third_block_pool(x)
+        #        print(x.shape, 'спуск 3')
+
+        x = self.forth_block(x)
+        #        print(x.shape, 'спуск 4')
+
+        x = self.forth_deconvblock(x)
+        x = copy_crop_concat(x, x3)
+        x = self.forth_convblock(x)
+
+        # # print(x.shape, 'подъем 2')
+        x = self.third_deconvblock(x)
+        x = copy_crop_concat(x, x2)
+        x = self.third_convblock(x)
+        # # print(x.shape, 'подъем 3')
+
+        x = self.second_deconvblock(x)
+        x = copy_crop_concat(x, x1)
+        x = self.second_convblock(x)
+        x = self.outp_layer(x)
+        return x
+
+    def foreground_extraction(self, picture):
+        w = picture.shape[1]
+        h = picture.shape[0]
+        pic = picture
+
+        pic = cv2.resize(pic, dsize=(320, 240), interpolation=cv2.INTER_CUBIC).transpose(2, 0, 1)
+        pic = torch.tensor(pic) / 255
+        result = self.model_custom(pic.to(DEVICE).unsqueeze(dim=0))['out'].squeeze(dim=0).detach().cpu()
+        result = 255 * (result > 0.5).squeeze(dim=0).numpy().astype(np.uint8)
+        pic = np.array(255 * pic).astype(np.uint8).transpose(1, 2, 0)
+        pic[result == 0] = 0
+        pic = cv2.resize(pic, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+        return pic
+
+
+class DeepLabResnet(nn.Module):
+    """Архитектура deeplabv3_resnet50 для бинарной сегментации (0 - фон, 1 - человек),
+    backbone - архитектура - Resnet50"""
+
+    def __init__(self):
+        super(DeepLabResnet, self).__init__()
+        self.model_custom = models.segmentation.deeplabv3_resnet50(pretrained=True)
+        for param in self.model_custom.parameters():
+            param.requires_grad = False
+        self.model_custom.classifier[4] = nn.Conv2d(256, 1, kernel_size=(1, 1), stride=(1, 1))
+
+    def forward(self, x):
+        return self.model_custom(x)['out']
+
+    def foreground_extraction(self, picture):
+        w = picture.shape[1]
+        h = picture.shape[0]
+        pic = picture
+
+        pic = cv2.resize(pic, dsize=(320, 240), interpolation=cv2.INTER_CUBIC).transpose(2, 0, 1)
+        pic = torch.tensor(pic) / 255
+        result = self.model_custom(pic.to(DEVICE).unsqueeze(dim=0))['out'].squeeze(dim=0).detach().cpu()
+        result = 255 * (result > 0.5).squeeze(dim=0).numpy().astype(np.uint8)
+        pic = np.array(255 * pic).astype(np.uint8).transpose(1, 2, 0)
+        pic[result == 0] = 0
+        pic = cv2.resize(pic, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+        return pic
